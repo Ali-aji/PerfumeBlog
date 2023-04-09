774d377c0920402d2828e86b9a90f5432a466b26
Ali Aji <ali.a.aji108@gmail.com>
2023-04-09 12:32:45 +0400

    add post views and urls.

--- perfumeblog/models.py
+++ perfumeblog/models.py
@@ -4,56 +4,60 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 # from ckeditor_uploader.fields import RichTextUploadingField
 from django.utils.text import slugify
+
+
 # from taggit.managers import TaggableManager
 
 # creating model manager
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PublishedManager,self).get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 # post model
 class Post(models.Model):
     STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    
+
     # image = models.ImageField(upload_to='featured_image/%Y/%m/%d/', blank=True, null=True) #this
 
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
-    body = models.TextField() # remove this 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()  # remove this
     # body=RichTextUploadingField() # add this
 
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
-    
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     # tags = TaggableManager()
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail',args=[self.slug])
-    
+        return reverse('blog:post_detail', args=[self.slug])
+
     # to get comment with parent is none and active is true, we can use this in template
     def get_comments(self):
         return self.comments.filter(parent=None).filter(active=True)
 
-# comment model    
+
+# comment model
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=50)
     email = models.EmailField()
     parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
@@ -65,7 +69,7 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ('created',)
-    
+
     def __str__(self):
         return self.body
 
